568beb248be10fe1b8b69268ef08fcc703ec4f96
ZenovEV <zwenv3@gmail.com>
2022-07-01 11:40:24 +0300

    Задача 4. Рабочий метод 2

--- Work_4.py
+++ Work_4.py
@@ -9,8 +9,6 @@ number_1 = int(input("Введите первое число:"))
 number_2 = int(input("Введите второе число:"))
 number_3 = int(input("Введите третье число:"))
 
-print("Положительных чисел", end=":")
-print((number_1 > 0) + (number_2 > 0) + (number_3 > 0))
-print("Отрицательных чисел", end=":")
-print((number_1 < 0) + (number_2 < 0) + (number_3 < 0))
+print("Положительных чисел:", (number_1 > 0) + (number_2 > 0) + (number_3 > 0))
+print("Отрицательных чисел:", (number_1 < 0) + (number_2 < 0) + (number_3 < 0))
 
