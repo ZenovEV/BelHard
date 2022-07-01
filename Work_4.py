4e7cac1d40762d5a925c545326a797cfe3520a2e
ZenovEV <zwenv3@gmail.com>
2022-07-01 10:46:20 +0300

    Задача 4. Находит положительные

--- Work_4.py
+++ Work_4.py
@@ -4,9 +4,11 @@
 Пользователь вводит 3 числа, сказать сколько из них положительных
 и сколько отрицательных
 '''
-number_1 = input("Введите первое число:").split()
-#number_2 = int(input("Введите второе число:"))
-#number_3 = int(input("Введите третье число:"))
-#number_1 = len(number_1.isdigite)
-#print(len(number_1))
-print(number_1)
+number_1 = int(input("Введите первое число:"))
+number_2 = int(input("Введите второе число:"))
+number_3 = int(input("Введите третье число:"))
+
+list = [number_1, number_2, number_3]
+
+itog = len([num for num in list if num >= 0])
+print(itog)
