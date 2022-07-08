b6b1604e3df504e05a1de214142cfa4efe4b4429
ZenovEV <zwenv3@gmail.com>
2022-07-08 08:31:58 +0300

    Задача 6.1 Тестовый вариант

---
+++ 6.1.1.py
@@ -0,0 +1,11 @@
+num_1 = int(input("Введите число N:"))
+num_2 = int(input("Введите число M:"))
+#num_3: int=input("Введите число K:")
+
+for i in range(0, num_2):
+    if(i % num_2 == 0):
+        print(f"Число {num_1} кратно {num_2}")
+else:
+    print(f"Число {num_1} некратно {num_2}")
+
+
